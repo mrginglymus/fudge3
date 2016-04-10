6418e3477c43b9f7dc80ff57a6b9af62d9b3d48a
Bill Collins <bilbocollins@gmail.com>
2016-04-10 21:02:48 +0100

    yoshi

--- src/test/java/works/bill/Fudge3ApplicationTests.java
+++ src/test/java/works/bill/Fudge3ApplicationTests.java
@@ -5,20 +5,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class Fudge3ApplicationTests {
-
-	private static final String USERNAME = "bilbocollins";
-	private static final String ACCESS_KEY = "6bcb2f57-3859-4b87-91a8-22582dfb2d41";
-	private static final String SAUCELABS_URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
 
 	private WebDriver driver;
 
@@ -27,12 +20,9 @@ public class Fudge3ApplicationTests {
 	private String baseUrl = "http://localhost:8080/fudge3-0.0.1-SNAPSHOT/";
 
 	@Before
-	public void spinUp() throws MalformedURLException {
-		DesiredCapabilities caps = DesiredCapabilities.chrome();
-		caps.setCapability("platform", "Windows 10");
-		caps.setCapability("version", "48.0");
-
-		driver = new RemoteWebDriver(new URL(SAUCELABS_URL), caps);
+	public void spinUp() {
+		driver = new HtmlUnitDriver();
+		wait = new WebDriverWait(driver, 60);
 	}
 
 	@After
@@ -41,16 +31,20 @@ public class Fudge3ApplicationTests {
 	}
 
 	@Test
-	public void testTitle() throws Exception {
-		driver.get("http://localhost:8080/");
-		System.out.println("********************************");
-		System.out.println(driver.getTitle());
-		System.out.println("********************************");
+	public void login() throws Exception {
 		driver.get(baseUrl);
-		System.out.println("********************************");
-		System.out.println(driver.getTitle());
-		System.out.println("********************************");
 		assertEquals("ICE", driver.findElement(By.cssSelector(".subtitle h1")).getText());
+
+		driver.findElement(By.cssSelector("a[href*='/things/1/']")).click();
+
+		assertEquals("Log In", driver.findElement(By.cssSelector(".subtitle h1")).getText());
+
+		driver.findElement(By.cssSelector("input[id*='username']")).sendKeys("user1");
+		driver.findElement(By.cssSelector("input[id*='password']")).sendKeys("user1");
+
+		driver.findElement(By.cssSelector("button")).click();
+
+		assertTrue(driver.findElement(By.cssSelector("div.alert-info")).isDisplayed());
 	}
 
 }
